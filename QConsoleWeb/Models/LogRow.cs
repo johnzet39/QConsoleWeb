358eac8aaf7082591f4df661092a0cad9625deac
Evgeniy Z <johnzet@yandex.ru>
2019-09-10 15:46:02 +0200

    Attributes.

--- QConsoleWeb/Models/LogRow.cs
+++ QConsoleWeb/Models/LogRow.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,14 +8,31 @@ namespace QConsoleWeb.Models
 {
     public class LogRow
     {
+        [Display(Name = "gid")]
         public string Gid { get; set; }
+
+        [Display(Name = "Операция")]
         public string Action { get; set; }
+
+        [Display(Name = "Пользователь")]
         public string Username { get; set; }
+
+        [Display(Name = "Адрес")]
         public string Address { get; set; }
+
+        [Display(Name = "Дата изменения")]
         public DateTime Timechange { get; set; }
+
+        [Display(Name = "Таблица")]
         public string Tablename { get; set; }
+
+        [Display(Name = "Схема")]
         public string Tableschema { get; set; }
+
+        [Display(Name = "gidnum")]
         public string Gidnum { get; set; }
+
+        [Display(Name = "Содежание")]
         public string Context { get; set; }
     }
 }
