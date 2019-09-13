39e60d132e3d44790b9c579069125236b4a041c9
Evgeniy Z <johnzet@yandex.ru>
2019-09-13 22:41:22 +0200

    Grants page backend.

---
+++ QConsoleWeb/Views/ViewModels/GrantViewModel.cs
@@ -0,0 +1,16 @@
+﻿using QConsoleWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QConsoleWeb.Views.ViewModels
+{
+    public class GrantViewModel
+    {
+        public List<User> UserList { get; set; }
+        public List<Grant> LayersList { get; set; }
+        public List<Grant> DictsList { get; set; }
+    }
+
+}
