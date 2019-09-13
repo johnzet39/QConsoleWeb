175abe6b20104e12889601248048e3f0cc6f3635
Evgeniy Z <johnzet@yandex.ru>
2019-09-13 22:39:05 +0200

    Start stat page.

---
+++ QConsoleWeb/Controllers/StatController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QConsoleWeb.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QConsoleWeb.Controllers
+{
+    public class StatController : Controller
+    {
+        private readonly ILoggerService _service;
+
+        public StatController(ILoggerService serv)
+        {
+            _service = serv;
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+    }
+}
