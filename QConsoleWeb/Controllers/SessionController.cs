fc8ebbb9859a839240b68d42a13914eaa2732dce
Evgeniy Z <johnzet@yandex.ru>
2019-10-10 17:29:39 +0200

    Update SessionController.cs

--- QConsoleWeb/Controllers/SessionController.cs
+++ QConsoleWeb/Controllers/SessionController.cs
@@ -11,7 +11,7 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace QConsoleWeb.Controllers
 {
-    //[Authorize]
+    [Authorize]
     public class SessionController : Controller
     {
         private ISessionService _service;
