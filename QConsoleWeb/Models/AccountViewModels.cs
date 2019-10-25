ef209e73ee28a31754ad987dcef5598469420ae7
Evgeniy Z <johnzet@yandex.ru>
2019-10-26 00:47:53 +0200

    Changes in alerts.

--- QConsoleWeb/Models/AccountViewModels.cs
+++ QConsoleWeb/Models/AccountViewModels.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Identity;
 
 namespace QConsoleWeb.Models
